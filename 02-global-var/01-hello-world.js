03c92a8b47740a8c6773156f6647b77fc7379716
gaohuan <3235218083@qq.com>
2020-09-18 17:26:01 +0800

    01-introduction demo code ok

---
+++ 02-global-var/01-hello-world.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const log=console.log;
+
+log(`dir name: ${__dirname}`);
+log(`file name:${__filename}`);
+
---
+++ 03-console/02-cookie-opt.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+console.time('TEST');
+longTask();
+console.timeEnd('TEST');
+
+
+function longTask(){
+  let n;
+   for(let i=0;i<10000;i++){
+     for(let j=0;j<10000;j++){
+       n=i*j;
+     }  
+  }
+   return n;
+}
